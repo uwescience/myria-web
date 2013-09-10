e677a85b5997030dde5b2e74affe3297e8d3d81b
Daniel Halperin <dhalperi@cs.washington.edu>
2013-09-10 13:14:27 -0700

    editor.js: autoformat
    Signed-off-by: Daniel Halperin <dhalperi@cs.washington.edu>

--- appengine/js/editor.js
+++ appengine/js/editor.js
@@ -1,58 +1,65 @@
 function handleerrors(request, display) {
-  request.success(function(result) {
-    var formatted = result.split("\n").join("<br>");
-    $(display).html(formatted);
-    //$(display).text(result);
-  });
+	request.success(function(result) {
+		var formatted = result.split("\n").join("<br>");
+		$(display).html(formatted);
+		// $(display).text(result);
+	});
 
-  request.error(function(jqXHR, textStatus, errorThrown) {
-    if (textStatus == 'timeout')
-      $(display).text("Server is not responding");
- 
-    if (textStatus == 'error')
-      var msg = '<div class="error"><a href="';
-      msg = msg + this.url;
-      msg = msg + '">Error</a></div>';
-      $(display).html(msg);
-  });
+	request.error(function(jqXHR, textStatus, errorThrown) {
+		if (textStatus == 'timeout')
+			$(display).text("Server is not responding");
+
+		if (textStatus == 'error')
+			var msg = '<div class="error"><a href="';
+		msg = msg + this.url;
+		msg = msg + '">Error</a></div>';
+		$(display).html(msg);
+	});
 }
 
 function getplan() {
-  var query = $("#query").val();
-  var request = $.get("plan", {query:query});
-  handleerrors(request, "#plan");
+	var query = $("#query").val();
+	var request = $.get("plan", {
+		query : query
+	});
+	handleerrors(request, "#plan");
 };
 
 function optimizeplan() {
-  getplan(); // make sure the plan matches the query
-  var query = $("#query").val();
-  var request = $.get("optimize", {query:query, target:"MyriaAlgebra"});
-  handleerrors(request, "#optimized");
+	getplan(); // make sure the plan matches the query
+	var query = $("#query").val();
+	var request = $.get("optimize", {
+		query : query,
+		target : "MyriaAlgebra"
+	});
+	handleerrors(request, "#optimized");
 }
 
 function compileplan() {
-  var query = $("#query").val();
-  var url = "compile?" + $.param({query:query, target:"MyriaAlgebra"});
-  document.location.href=url;
+	var query = $("#query").val();
+	var url = "compile?" + $.param({
+		query : query,
+		target : "MyriaAlgebra"
+	});
+	document.location.href = url;
 }
 
-$(document).ready(function(){
-  $("#query").bind('keyup change', function() {
-  $(".display").empty();
+$(document).ready(function() {
+	$("#query").bind('keyup change', function() {
+		$(".display").empty();
+	});
+	$(".planner").click(optimizeplan);
+	$(".compiler").click(compileplan);
+	$(".example").click(function() {
+		$(".display").empty();
+		var example_query = $(this).text();
+		$("#query").val(example_query);
+		optimizeplan();
+	});
+	$(".display").css("border-style", "solid");
+	$(".error").css("font-color", "red");
+	$(".label").css("font-size", "small");
+	$(".label").css("font-style", "italic");
+	$(".display").css("width", 600);
+	$(".display").css("height", 100);
 });
-  $(".planner").click(optimizeplan);
-  $(".compiler").click(compileplan);
-  $(".example").click(function(){
-$(".display").empty();
-var example_query = $(this).text();
-$("#query").val(example_query);
-optimizeplan();
-});
-  $(".display").css("border-style", "solid");
-  $(".error").css("font-color", "red");
-  $(".label").css("font-size", "small");
-  $(".label").css("font-style", "italic");
-  $(".display").css("width", 600);
-  $(".display").css("height", 100);
-});
-
