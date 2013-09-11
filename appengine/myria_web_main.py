af9284f06857897a2f96974429d0f55247d4f1c2
Daniel Halperin <dhalperi@cs.washington.edu>
2013-09-11 00:33:27 -0700

    app.yaml: upgrade to webapp2 latest
    The reason that the `Debug=True` option wasn't triggering the stack
    trace dump is GAE defaults to an old, broken version of the webapp2
    library. Upgrade, then undo the manual override in myria_web_main.py
    
    Signed-off-by: Daniel Halperin <dhalperi@cs.washington.edu>

--- appengine/myria_web_main.py
+++ appengine/myria_web_main.py
@@ -4,7 +4,6 @@ from raco.myrialang import compile_to_json
 from raco.viz import plan_to_dot
 from google.appengine.ext.webapp import template
 import os.path
-import traceback
 import webapp2
 
 defaultquery = """A(x) :- R(x,3)"""
@@ -18,17 +17,7 @@ def programplan(query,target):
 def format_rule(expressions):
     return "\n".join(["%s = %s" % e for e in expressions])
 
-class RequestHandlerBase(webapp2.RequestHandler):
-    def handle_exception(self, exception, debug_mode):
-        if isinstance(exception, webapp2.HTTPException):
-            self.response.set_status(exception.code)
-            self.response.out.write(str(exception))
-        else:
-            self.response.set_status(500)
-            self.response.out.write(traceback.format_exc(exception))
-        self.response.headers['Content-Type'] = 'text/plain'
-        
-class MainPage(RequestHandlerBase):
+class MainPage(webapp2.RequestHandler):
     def get(self,query=defaultquery):
 
         dlog = RACompiler()
@@ -43,7 +32,7 @@ class MainPage(RequestHandlerBase):
     
         self.response.out.write(template.render(path, locals()))
 
-class Plan(RequestHandlerBase):
+class Plan(webapp2.RequestHandler):
     def get(self):
         query = self.request.get("query")
         dlog = RACompiler()
@@ -53,7 +42,7 @@ class Plan(RequestHandlerBase):
         self.response.headers['Content-Type'] = 'text/plain'
         self.response.write(plan)
 
-class Optimize(RequestHandlerBase):
+class Optimize(webapp2.RequestHandler):
     def get(self):
         query = self.request.get("query")
     
@@ -67,7 +56,7 @@ class Optimize(RequestHandlerBase):
         self.response.headers['Content-Type'] = 'text/plain'
         self.response.write(optimized)
 
-class Compile(RequestHandlerBase):
+class Compile(webapp2.RequestHandler):
     def get(self):
         query = self.request.get("query")
     
@@ -84,7 +73,7 @@ class Compile(RequestHandlerBase):
         self.response.headers['Content-Type'] = 'application/json'
         self.response.write(compiled)
 
-class Dot(RequestHandlerBase):
+class Dot(webapp2.RequestHandler):
     def get(self):
         query = self.request.get("query")
         svg_type = self.request.get("type")
