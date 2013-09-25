d7d0705383f5de7d33fd7f7576c614719f8c1bb4
Daniel Halperin <dhalperi@cs.washington.edu>
2013-09-25 10:41:38 -0700

    editor: move examples to webapp and dynamically render them
    This is groundwork for adding support for other languages
    
    Signed-off-by: Daniel Halperin <dhalperi@cs.washington.edu>

--- appengine/myria_web_main.py
+++ appengine/myria_web_main.py
@@ -139,6 +139,41 @@ class Datasets(MyriaPage):
 
 class Editor(MyriaPage):
     def get(self, query=defaultquery):
+        # Examples is a dictionary from language -> [pairs]. Each pair is (Label, Code).
+        datalog_examples = [
+          ('Select', '''A(x) :- R(x,3)'''),
+          ('Select2', '''A(x) :- R(x,y),S(y,z,4),z<3'''),
+          ('Self-join', '''A(x,z) :- R(x,y),R(y,z)'''),
+          ('Triangle', '''A(x,z) :- R(x,y),S(y,z),T(z,x)'''),
+          ('Cross Product', '''A(x,z) :- S(x),T(z)'''),
+          ('Two cycles', 'A(x,z) :- R(x,y),S(y,a,z),T(z,b,x),W(a,b)'),
+          ('Two Chained Rules', 'A(x,z) :- R(x,y,z)\n\nB(w) :- A(3,w)'),
+          ('Two Independent Rules', 'A(x,z) :- R(x,y,z)\n\nB(w) :- C(3,w)'),
+          ('Project TwitterK', 'JustX(x) :- TwitterK(x,y)'),
+          ('Self Join TwitterK', 'SelfJoin(x,z) :- TwitterK(x,y), TwitterK(y,z)'),
+          ('In Degrees from TwitterK', 'InDegree(x, COUNT(y)) :- TwitterK(x,y)'),
+          ('Two Hops Count in TwitterK', 'TwoHopsCountK(x,z,COUNT(y)) :- TwitterK(x,y), TwitterK(y,z)'),
+          ('Triangles TwitterK', 'Triangles(x,y,z) :- TwitterK(x,y), TwitterK(y,z), TwitterK(z,x)'),
+          ('NCCDC Filtered to Attack Window', '''attackwindow(src, dst, time) :-
+    nccdc(src,dst,proto,time, x, y, z)
+    , time > 1366475761
+    , time < 1366475821'''),
+          ('NCCDC DDOS Victims', '''InDegree(dst, count(time)) :- nccdc(src, dst, proto, time, x, y, z)
+
+Victim(dst) :- InDegree(dst, cnt), cnt > 10000'''),
+          ('SP2Bench Q10', '''Q10(subject, predicate) :-
+    sp2bench_1m(subject, predicate, 'person:Paul_Erdoes')'''),
+          ('SP2Bench Q3a', '''Q3a(article) :-
+    sp2bench_1m(article, 'rdf:type', 'bench:Article')
+    , sp2bench_1m(article, 'swrc:pages', value)'''),
+          ('SP2Bench Q1', '''Q1(yr) :-
+    sp2bench_1m(journal, 'rdf:type', 'bench:Journal')
+    , sp2bench_1m(journal, 'dc:title', 'Journal 1 (1940)')
+    , sp2bench_1m(journal, 'dcterms:issued', yr)''')
+        ]
+
+        examples = { 'datalog' : datalog_examples }
+
         dlog = RACompiler()
         dlog.fromDatalog(query)
         plan = format_rule(dlog.logicalplan)
