03b186bfb9e6549170ac4806e156e3e64f502e22
Dominik Moritz <domoritz@gmail.com>
2014-02-25 19:39:24 -0800

    Remove states to util code

--- appengine/tests/test_visualization.py
+++
@@ -1,8 +0,0 @@
-from nose.tools import assert_equal
-import states_to_utilization as conv
-from data import EXAMPLE_DETAILS, EXAMPLE_UTILIZATION
-
-
-def test_conversion():
-    utilization = conv.get_utilization(EXAMPLE_DETAILS)
-    assert_equal(utilization, EXAMPLE_UTILIZATION)
