b26f9ad28361c1981494fa5327b89e48f7437ff9
moreau <moreau@d-173-250-189-117.dhcp4.washington.edu>
2014-02-26 17:32:45 -0800

    fixed html element bug, graph.js is a WIP

--- appengine/js/graph.js
+++ appengine/js/graph.js
@@ -2,7 +2,7 @@ var graph = function (element, queryPlan) {
     // do all the chart stuff
     debug(queryPlan);
 
-    var chartElement = d3.select('.some-class');
+    var chartElement = d3.select('.chart');
 
     var networkVis = networkVisualization(chartElement, [], queryPlan);
 
@@ -10,9 +10,36 @@ var graph = function (element, queryPlan) {
 
     var fragmentVis = fragmentVisualization(chartElement, 0, queryPlan);
 
-    // var url =  'http://' + myriaConnection + '/query/query-' + queryId;
 
-    // $.getJSON(url, function(data) {
-    //     debug(data)
-    // });
+    // Process the queryPlan
+    var fragments = queryPlan.physicalPlan.fragments;
+    for(var i=0; i<fragments.length; i++){
+        var obj = fragments[i];
+        getNode(obj);
+    }
+
 };
+
+
+
+function getNode(fragment)
+{
+    var operators = fragment.operators;
+    var workers = fragment.workers;
+    var id = fragment.fragmentIndex;
+    var pid = null
+
+    for(var i=0; i<operators.length; i++){
+        var obj = operators[i];
+        if (obj.hasOwnProperty('argPf')) {
+            pid = obj.argPf.index;
+        } 
+    }
+
+    if (pid) {
+        console.log("Fragment %d has parent %d", id, pid);
+    } else {
+        console.log("Fragment %d has no parents", id);
+    }
+
+}
