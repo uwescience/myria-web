0a78ad02e1d003c737c79e3f623e9045bfc8cbbc
Umar Javed <ujaved@warturtle.dyn.cs.washington.edu>
2014-03-04 13:28:11 -0800

    matrix labels and popover

--- appengine/js/networkvis.js
+++ appengine/js/networkvis.js
@@ -6,6 +6,7 @@ var networkVisualization = function (element, fragments, queryPlan) {
         fragment: fragmentId
     });
 
+    //url = "data/data.csv";
     d3.csv(url, function (data) {
     	var matrix = [],
       		dataset = [],
@@ -31,9 +32,11 @@ var networkVisualization = function (element, fragments, queryPlan) {
 
       	var workers = d3.keys(workers);
       	workerID2matrixID = new Object();
+      	matrixID2workerID = new Object();
   		matrix = new Array(workers.length);
   		for (var i = 0; i < matrix.length; i++) {
   			workerID2matrixID[workers[i]] = i;
+  			matrixID2workerID[i] = workers[i];
     		matrix[i] = new Array(workers.length);
     		for (var j = 0; j < matrix[i].length; j++) {
       			matrix[i][j] = 0;
@@ -48,13 +51,13 @@ var networkVisualization = function (element, fragments, queryPlan) {
   		});
 
 
-        draw(matrix);
+        draw(matrix,workers,matrixID2workerID);
     });
 
-    function draw(matrix) {
+    function draw(matrix,workers,matrixID2workerID) {
     	debug(element.style('width'));
-        var margin = {top: 10, right: 10, bottom: 60, left:20 },
-            matMargin = {top: 10, right: 10, bottom: 60, left:20 },
+        var margin = {top: 10, right: 10, bottom: 10, left:10 },
+            matMargin = {top: 10, right: 10, bottom: 10, left:10 },
         	width = parseInt(element.style('width'), 10),
         	height = 600,
             chartWidth = width - margin.left - margin.right,
@@ -65,8 +68,6 @@ var networkVisualization = function (element, fragments, queryPlan) {
         var matrixScale = d3.scale.ordinal()
           .domain(_.range(matrix.length))
           .rangeBands([0, matrixWidth], .1);
-
-       foo = matrixScale;
 
       // converts a matrix into a sparse-like entries
       // maybe 'expensive' for large matrices, but helps keeping code clean
@@ -89,9 +90,11 @@ var networkVisualization = function (element, fragments, queryPlan) {
             .attr("width", width)
             .attr("height", height);
 
+        /*
         var tp = element.append('div')
     	    .attr('class', 'tooltip')
     	    .style("opacity", 0.3);
+    	*/
         
         var chart = svg.append("g")
             .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
@@ -109,11 +112,52 @@ var networkVisualization = function (element, fragments, queryPlan) {
               .attr('height', matrixScale.rangeBand())
               .attr('y', function(d){return matrixScale(d.i);})
               .attr('x', function(d){return matrixScale(d.j);})
-              .style('fill',function(d){ return color(d.val);});
+              .style('fill',function(d){ return color(d.val);})
+              .popover(function(d) {
+              	//var content = boxTemplate({numTuples: d.val});
+              	var src = matrixID2workerID[d.i];
+              	var dst = matrixID2workerID[d.j];
+               	return {
+                    title: "src worker ID:" + src + "\n dst worker ID:" + dst,
+                    content: "total # of tuples: " + d.val
+                    //content: content
+                };
+              });
               //.on('mouseover', function(d){pixel_mouseover(d);})
               //.on('mouseout', function(d){mouseout(d);});
               // .on('click', function(d){reorder_matrix(d.i, 'col'); reorder_matrix(d.j, 'row');});
-              //the last thing works only for symmetric matrices, but with asymmetric sorting
+
+        
+        var tick_col = chart.append('g')
+                        .attr('class','ticks')
+                        //.attr('transform', 'translate(' + (label_space + 10) + ',' + (label_space) + ')')
+      	                .selectAll('text.tick')
+                        .data(workers);
+
+        tick_col.enter()
+      		.append('text')
+          	.attr('class','tick')
+          	.style('text-anchor', 'start')
+          	//.attr('transform', function(d, i){return 'rotate(270 ' + scale(order_col[i] + 0.7) + ',0)';})
+          	//.attr('font-size', scale(0.8))
+          	.text(function(d){ return d; })
+          	.attr('x', function(d, i){return matrixScale(i);});
+
+        var tick_row = chart.append('g')
+                          .attr('class','ticks')
+                        //.attr('transform', 'translate(' + (label_space) + ',' + (label_space + 10) + ')')
+                          .selectAll('text.tick')
+                          .data(workers);
+
+        tick_row.enter()
+                .append('text')
+                .attr('class','tick')
+                .style('text-anchor', 'end')
+                //.attr('font-size', scale(0.8))
+                .text(function(d){ return d; })
+                .attr('y', function(d, i){return matrixScale(i);});
+          
+         
     }
 
     // return variables that are needed outside this scope
