8a5435ef23d7f247b816ab68d575fb198c05d8e3
Shumo Chu <shumo.chu@gmail.com>
2014-04-07 21:25:27 -0700

    add back to top button.

--- appengine/js/editor.js
+++ appengine/js/editor.js
@@ -251,6 +251,23 @@ function searchDataset(e) {
 }
 
 $(function() {
+  //back to top button
+  var offset = 220;
+  var duration = 300;
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > offset) {
+        $('.back-to-top').fadeIn(duration);
+    } else {
+        $('.back-to-top').fadeOut(duration);
+    }
+  });
+    
+  $('.back-to-top').click(function(event) {
+    event.preventDefault();
+        $('html, body').animate({scrollTop: 0}, duration);
+        return false;
+  });
+  
   resetResults();
 
   editor.on("change", resetResults);
