3027098b6eb507b597eb85a1ed4812530ef3c213
Daniel Halperin <dhalperi@cs.washington.edu>
2014-04-23 10:55:32 -0700

    deploy.sh: verify on production branch

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-if [ -z "$(git status -s)" ]
+cur_status="$(git status -s)"
+cur_branch="$(git rev-parse --abbrev-ref HEAD)"
+if [ -n "$cur_status" ]
 then
+	echo "myria-web has been modified; cannot deploy"
+elif [ "production" != "$cur_branch" ]
+then
+	echo "myria-web is not on the production branch; cannot deploy"
+else
 	git rev-parse HEAD > appengine/VERSION && \
 	appcfg.py --oauth2 update appengine
-else
-	echo "myria-web has been modified; cannot deploy"
 fi
