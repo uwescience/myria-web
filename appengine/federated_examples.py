f3f08e231f4964aaf6fe144f65e33a745cb8f227
Andrew Whitaker <whitaker@cs.washington.edu>
2014-07-01 09:49:08 -0700

    Combined myrial, bigdog example

--- appengine/federated_examples.py
+++ appengine/federated_examples.py
@@ -1,11 +1,21 @@
 
 simple_bd = """connect(%afl, "vega.cs.washington.edu:7777");
 
-%afl("b2 = filter(b1, val > 1000)");
+%afl("store(filter(B1, data > 0), B4)");
+"""
+
+combined_bd = """connect(%afl, "vega.cs.washington.edu:7777");
+
+X = scan(public:adhoc:sc_points);
+Y = [from X where X.v > 0 emit *];
+store(Y, public:adhoc:bd_output);
+
+%afl("store(filter(B1, data > 0), B4)");
 """
 
 __myrial_examples = [
-    ('Simple bigdog query', simple_bd)
+    ('Simple AFL query', simple_bd),
+    ('AFL + Myrial query', combined_bd)
 ]
 
 federated_examples = {
