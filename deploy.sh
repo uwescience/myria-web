7e9efda57ebe8fd83a2902f4fe1ab57b26c7a6c4
Andrew Whitaker <whitaker@cs.washington.edu>
2014-06-30 17:15:14 -0700

    use federated branch in deploy

--- deploy.sh
+++ deploy.sh
@@ -5,9 +5,9 @@ cur_branch="$(git rev-parse --abbrev-ref HEAD)"
 if [ -n "$cur_status" ]
 then
 	echo "myria-web has been modified; cannot deploy"
-elif [ "production" != "$cur_branch" ]
+elif [ "federated" != "$cur_branch" ]
 then
-	echo "myria-web is not on the production branch; cannot deploy"
+	echo "myria-web is not on the federated branch; cannot deploy"
 else
 	git rev-parse HEAD > appengine/VERSION && \
 	git rev-parse --abbrev-ref HEAD > appengine/BRANCH && \
