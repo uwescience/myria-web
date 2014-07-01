872cff2aa5656684bb308e89bf2e67a77ae68201
TheYorkWei <york.wei@gmail.com>
2014-07-01 12:18:31 -0700

    returns correct results

--- clangtestserv/nodeserver/server.js
+++ clangtestserv/nodeserver/server.js
@@ -2,16 +2,35 @@ var http = require('http');
 var qs = require("querystring");
 var fs = require('fs');
 var cp = require('child_process');
+var url = require('url');
 
-var url = 'http://localhost:8080/compile?query=A(x)+%3A-+R2(x%2C3)&language=datalog&backend=clang';
 var filepath = '../../submodules/raco/c_test_environment/';
-
 var counter = 0;
-var myriares;
+
 http.createServer(function (req, res) {
+    parseQuery(req, res);
+
+}).listen(1337, 'localhost');
+console.log('Server running at http://localhost:1337/');
+
+function getJSON(req, res, qid, start) {
+    var end = new Date().getTime();
+    var query_status = {url : 'http://localhost:1337/query?qid='+qid,
+		       startTime: start, finishTime: end, status: "SUCCESS",
+		       queryId: qid};
+    res.writeHead(200, {'Content-Type': 'application/json', 'Location':
+			'http://localhost:1337/query?qid='+qid});
+    res.write(JSON.stringify(query_status));
+    res.end();
+}
+	
+
+// Parses the query from posted json
+function parseQuery(req, res) {
+    var start = new Date().getTime();
     var plan;
     var qid = counter;
-    console.log("wait");
+    console.log("waiting");
     if (req.method == "POST") {
 	console.log('post');
 	var body = '';
@@ -19,7 +38,7 @@ http.createServer(function (req, res) {
             body += chunk;
 	});
 	req.on('end', function() {
-	    myriares = JSON.parse(body);
+	    var myriares = JSON.parse(body);
 	    
 	    plan = myriares['plan'];
 	    fs.writeFile(filepath + 'q' + qid + ".cpp", plan, function(err) {
@@ -29,19 +48,18 @@ http.createServer(function (req, res) {
 	    });
 	});
 
-	runClang(qid);
+	runClang(req, res, qid, start);
 
 	counter++;
+   } else {
+       res.writeHead(400, {'Content-Type': 'text/html'});
+       res.write("nothing?");
+       res.end();
    }
+}
 
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write("success");
-
-    res.end();
-}).listen(1337, 'localhost');
-console.log('Server running at http://localhost:1337/');
-
-function runClang(qid) {
+// runs clang on server
+function runClang(req, res, qid, start) {
     var filename = 'q' + qid;
     var options = { encoding: 'utf8', timeout: 0, maxBuffer: 200*1024,
 		    killSignal: 'SIGTERM', cwd: filepath, env: null };
@@ -53,36 +71,7 @@ function runClang(qid) {
 	    console.log('exec error: ' + error);
 	}
 	console.log(filename + ' done');
+	getJSON(req, res, qid, start);
     });
 }
 
-function test() {
-var postHTML = 
-  '<html><head><title>Post Example</title></head>' +
-  '<body>' +
-  '<form method="post">' +
-  'Input 1: <input name="qid"><br>' +
-  'Input 2: <input name="plan"><br>' +
-  '<input type="submit">' +
-  '</form>' +
-  '</body></html>';
-
-    var body = "";
-    if (req.method == 'POST') {
-	req.on('data', function(chunk) {
-            body += chunk;
-	});
-	req.on('end', function() {
-	    var variables = qs.parse(body);
-	    console.log('post ' + body + " " + variables['qid']);
-	    res.writeHead(200, {'Content-Type': 'text/html'});
-	    res.write('<p>' + variables['qid'] + ' ' + variables['plan']
-		      + '</p>');
-	    res.end(postHTML);
-	});
-   } else if (req.method == 'GET') {
-       console.log('get');
-       res.writeHead(200, {'Content-Type': 'text/html'});
-       res.end(postHTML);
-   }
-}
