4047d8627a6ded510eaedac20115a3599fe72f2b
TheYorkWei <york.wei@gmail.com>
2014-07-01 18:30:25 -0700

    creating sqlconnection w/ nodejs

--- clangtestserv/nodeserver/server.js
+++ clangtestserv/nodeserver/server.js
@@ -3,15 +3,39 @@ var qs = require("querystring");
 var fs = require('fs');
 var cp = require('child_process');
 var url = require('url');
+var sqlite = require("sqlite3").verbose();
 
 var filepath = '../../submodules/raco/c_test_environment/';
 var counter = 0;
 
 http.createServer(function (req, res) {
-    parseQuery(req, res);
+    var path = url.parse(req.url).pathname;
+
+    switch(path) {
+	case '/dataset':
+	  accessDataset(req, res);
+	break;
+	default:
+          parseQuery(req, res);
+	break;
+    }
 
 }).listen(1337, 'localhost');
 console.log('Server running at http://localhost:1337/');
+
+function accessDataset(req, res) {
+    var file = 'dataset.db';
+    var exists = fs.existsSync(file);
+    if (exists) {
+	var db = new sqlite.Database(file);
+	
+    } else {
+       res.writeHead(404, {'Content-Type': 'text/html'});
+       res.write("database file not found");
+       res.end();
+    }
+
+}
 
 function getJSON(req, res, qid, start) {
     var end = new Date().getTime();
@@ -41,7 +65,7 @@ function parseQuery(req, res) {
 	    var myriares = JSON.parse(body);
 	    
 	    plan = myriares['plan'];
-	    fs.writeFile(filepath + 'q' + qid + ".cpp", plan, function(err) {
+	    fs.writeFile(filepath +'q'+ qid +".cpp", plan, function(err) {
 		if (err) {
 		    console.log(err);
 		}
@@ -66,12 +90,12 @@ function runClang(req, res, qid, start) {
     var cmd = 'python runclang.py clang ' + filename;
     cp.exec(cmd, options, function(error, stdout, stderr) {
 	console.log('stdout: ' + stdout);
-	console.log('stderr: ' + stderr);
 	if (error !== null) {
-	    console.log('exec error: ' + error);
+	    console.log('error: ' + error);
+	} else {
+	    console.log(filename + ' done');
+	    getJSON(req, res, qid, start);
 	}
-	console.log(filename + ' done');
-	getJSON(req, res, qid, start);
     });
 }
 
