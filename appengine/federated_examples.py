c665df67341bcccb9f87801cd76574c43df2fbd1
Andrew Whitaker <whitaker@cs.washington.edu>
2014-07-14 18:02:22 -0700

    Add simple join example

--- appengine/federated_examples.py
+++ appengine/federated_examples.py
@@ -13,7 +13,18 @@ store(Y, public:adhoc:bd_output);
 %afl("store(filter(B1, data > 0), B4)");
 """
 
+simple_join = '''Dept = scan(public:adhoc:department);
+Emp = scan(public:adhoc:employee);
+
+EmpDept = [from Emp, Dept
+           where Emp.dept_id=Dept.id
+           emit Emp.*, Dept.name as dept_name];
+Rich = [from EmpDept where salary > 6000 emit name, dept_name];
+
+store(Rich, myrial_output);'''
+
 __myrial_examples = [
+    ('Simple join', simple_join),
     ('Simple AFL query', simple_bd),
     ('AFL + Myrial query', combined_bd)
 ]
