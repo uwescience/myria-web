6139786d27a63b6c73065aea7d85c57e14becbe2
TheYorkWei <york.wei@gmail.com>
2014-09-24 17:37:53 -0700

    loading myria queries in js

--- appengine/js/queries.js
+++ appengine/js/queries.js
@@ -1,30 +1,9 @@
 //query templates go here
-var query_templates = {};
-/*  {% for q in queries %}
-			<tr class="{{q.bootstrapStatus}} query-row" data-status="{{q.status}}" data-id="{{q.queryId}}">
-				<td class="query-url"><a href="{{q.url}}" target="_blank">{{q.queryId}}</a></td>
-				<td class="query-raw">{{q.rawQuery}}</td>
-				<td class="query-status">{{q.status}}</td>
-				<td>
-					{% if q.profilingMode %}
-						{% if q.status == 'SUCCESS' %}
-							<a href="/profile?queryId={{q.queryId}}" class="glyphicon glyphicon-dashboard" title="Visualization of query profiling" data-toggle="tooltip"></a>
-						{% elif q.status in ['RUNNING', 'ACCEPTED'] %}
-							<span class="glyphicon glyphicon-dashboard" title="Visualization will be available when the query has finished" data-toggle="tooltip"></span>
-						{% else %}
-							<span class="glyphicon glyphicon-dashboard" title="Visualization not available for failed queries" data-toggle="tooltip"></span>
-						{% endif %}
-					{% else %}
-						<span title="Profiling not enabled for this query" data-toggle="tooltip">-</span>
-					{% endif %}
-				</td>
-				<td class="query-elapsed">{{q.elapsedStr}}</td>
-				<td class="query-finish">
-					<abbr class="timeago" title="{{q.finishTime}}">{{q.finishTime}}</abbr>
-				</td>
-			</tr>
-			{% endfor %}
-*/
+var query_templates = {
+  queryInfo: _.template('<tr class="<%- bootstrapStatus %> query-row" data-status="<%- status %>" data-id="<%- queryId %>"> <td class="query-url"><a href="<%- url%>" target="_blank"><%- queryId %></a></td><td class="query-raw"><%- rawQuery %></td><td class="query-status"><%- status %></td>'),
+  profileInfo: _.template('<td><% if (profilingMode) {%><% if (status == "SUCCESS") { %><a href="/profile?queryId=<%- queryId %>" class="glyphicon glyphicon-dashboard" title="Visualization of query profiling" data-toggle="tooltip"></a><% } else if (status == "RUNNING" || status == "ACCEPTED") {%><span class="glyphicon glyphicon-dashboard" title="Visualization will be available when the query has finished" data-toggle="tooltip"></span><% } else {%><span class="glyphicon glyphicon-dashboard" title="Visualization not available for failed queries" data-toggle="tooltip"></span><% } %><% } else { %><span title="Profiling not enabled for this query" data-toggle="tooltip">-</span><% } %></td>'),
+  finishInfo: _.template('<td class="query-elapsed"><%- elapsedStr %></td><td class="query-finish"><abbr class="timeago" title="<%- finishTime %>"><%- finishTime%></abbr></td></tr>')
+};
 
 var editorBackendKey = 'myria',
     backendProcess = 'myria',
@@ -57,14 +36,34 @@ function loadTable() {
   request.success(function (info) {
     var conn = JSON.parse(info).connection;
     if (backendProcess == 'clang') {
-      url = conn + '/dataset?backend=clang';
+      url = conn + '/queries?backend=clang';
     }
     else if (backendProcess == 'grappa') {
-      url = conn + '/dataset?backend=grappa';
+      url = conn + '/queries?backend=grappa';
     } else {
-      url = conn + '/dataset';
+      url = conn + '/query';
     }
- });
+    var t = query_templates;
+    var jqxhr = $.getJSON(url, function (data) {
+      var html = '';
+      _.each(data, function (d) {
+	var qload = '';
+        if (_.contains(grappaends, backendProcess)) {
+	  qload = '/query?qid=' + d['queryId'];
+	}
+        html += t.queryInfo({bootstrapStatus: d.bootstrapStatus,
+                             status: d.status, queryId: d.queryId,
+                             rawQuery: d.rawQuery, url: d.url + qload});
+        html += t.profileInfo({profilingMode: d.profilingMode,
+                                 status: d.status, queryId: d.queryId});
+        html += t.finishInfo({elapsedStr: d.elaspedStr,
+                              finishTime: d.finishTime});
+        });
+      $("#querytable").html(html);
+    }).fail (function (res, err) {
+      console.log(err);
+    });
+  });
 }
 
 function saveState() {
@@ -91,7 +90,7 @@ $(function() {
   setInterval(saveState, 2000);
 
   loadTable();
-  
+
   $('.query-row[data-status="RUNNING"]').each(function(i, e) {
     var qid = $(this).attr('data-id');
     window.setInterval(function() {
